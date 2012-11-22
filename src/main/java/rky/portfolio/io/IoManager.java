d317c148a40bdd7eb31c3bfb45eb91f3c78efa63
rshepherd <me@rshepherd.org>
2012-11-22 12:41:44 -0500

    Removing player property from IoManager

--- src/main/java/rky/portfolio/io/IoManager.java
+++ src/main/java/rky/portfolio/io/IoManager.java
@@ -7,45 +7,39 @@ import rky.portfolio.Player.Players;
 
 public class IoManager
 {
-	private final Server server;
-	private Players players;
-	private List<MessageListener> listeners;
+    private final Server          server;
+    private List<MessageListener> listeners;
 
-	public IoManager(int port)
-	{
-		server = new Server(port);
-	}
+    public IoManager(int port)
+    {
+        server = new Server(port);
+    }
 
-	public Players connect(int numPlayers)
-	{
-	    return server.start(numPlayers);
-	}
+    public Players connect(int numPlayers)
+    {
+        return server.start(numPlayers);
+    }
 
-	public void send(Player p, Message m)
-	{
-		System.out.println("S->" + p.name + ": " + m);
-		broadcastToListeners(p, m, true);
-		server.send(p, m.toString());
-	}
+    public void send(Player p, Message m)
+    {
+        System.out.println("S->" + p.name + ": " + m);
+        broadcastToListeners(p, m, true);
+        server.send(p, m.toString());
+    }
 
-	public Message receive(Player p)
-	{
-		Message m = new Message(server.receive(p));
-		broadcastToListeners(p, m, false);
-		System.out.println(p.name + "->S" +  ": " + m);
-		return m;
-	}
+    public Message receive(Player p)
+    {
+        Message m = new Message(server.receive(p));
+        broadcastToListeners(p, m, false);
+        System.out.println(p.name + "->S" + ": " + m);
+        return m;
+    }
 
-	public Players getPlayers()
-	{
-		return players;
-	}
-	
     public void registerListener(MessageListener listener)
     {
         listeners.add(listener);
     }
-    
+
     private void broadcastToListeners(Player p, Message m, boolean outgoing)
     {
         for (MessageListener l : listeners)
